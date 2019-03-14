63ff6ada5f6996672698f8528066514bf892dc27
joniba <jbenami1@gmail.com>
2019-03-14 23:36:42 +0200

    use any logger; temporarily print entire req/res if silly requested until contents are better understood

--- example.js
+++ example.js
@@ -1,5 +1,8 @@
-var request = require('superagent');
-var logger = require('./');
+const request = {};//require('superagent');
+const debug = require('debug')('ros:http');
+const silly = require('debug')('ros:http:silly');
+const initLogger = require('./');
+const logger = initLogger({ logger: debug, outgoing: true, requestLogger: silly, responseLogger: silly });
 
-request.get('https://github.com/').use(logger).query({q: 'search'}).end();
+request.get('https://github.com/').use(logger).query({ q: 'search' }).end();
 request.post('https://github.com/').use(logger).end();
